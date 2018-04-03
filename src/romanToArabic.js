261e6d307349de07545ab50a0766017ba178e532
Marcin Mieszek <marcin.mieszek@gmail.com>
2018-04-03 08:27:00 +0200

    Add initial romanToArabic conversion function

---
+++ src/romanToArabic.js
@@ -0,0 +1,7 @@
+const conversionTable = require('./conversionTable')
+
+function romanToArabic(roman) {
+  const 'I'
+}
+
+module.exports = arabicToRoman
---
+++ src/romanToArabic.test.js
@@ -0,0 +1,9 @@
+const romanToArabic = require('./romanToArabic')
+
+test('I equals 1', () => {
+  expect(romanToArabic('I')).toBe(1)
+})
+
+test('V equals 5', () => {
+  expect(romanToArabic('V')).toBe('5)
+})
